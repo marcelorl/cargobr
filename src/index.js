3f5fb6415f6715f950e128ca800e74d13f2ee006
Marcelo rizzardo Lima <marcelo@quicktech.no>
2016-09-10 17:58:48 -0300

    Functional example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import 'babel-polyfill'
 import React from 'react'
 import {render} from 'react-dom'
+import {createStore} from 'redux'
 import {Provider} from 'react-redux'
-import {Router, browserHistory} from 'react-router'
+import App from './components/App'
+import reducer from './reducers'
+
+const store = createStore(reducer)
 
 render(
-	<div>test</div>,
-	document.getElementById('app')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
 )
